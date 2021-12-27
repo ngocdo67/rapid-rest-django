eaf6db1573faf144d321ab570febf8f0255df951
Norah Do <ndo33@gatech.edu>
2021-12-27 14:25:11 +0800

    Setup

--- rapidapipractice/api/settings.py
+++ rapidapipractice/api/settings.py
@@ -11,5 +11,8 @@ INSTALLED_APPS = [
 
 # Heroku: Update database configuration from $DATABASE_URL. 
 import dj_database_url 
+import os
+
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 db_from_env = dj_database_url.config(conn_max_age=500) 
 DATABASES['default'].update(db_from_env)
